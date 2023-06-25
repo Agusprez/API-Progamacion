59d38d63f6cfbbc0e60c0fdb36f028ba0c21c692
Perez Agustin Gabriel <perezagustin95@hotmail.com>
2023-06-24 22:18:31 -0300

    Borre lineas en blanco para que quede bien en la entrega

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,5 +1,4 @@
 //Sabiendo el input del formulario y que solo tiene espacios en blanco, colorea con rojo al borde, mientras que si tiene texto valido, lo colorea con verde
-
 function colorearCampos(valorID) {
   let valor = $.trim($(valorID).val());
   $(valorID).css("border-color", valor == "" ? "red" : "green");
@@ -11,11 +10,9 @@ $("#producto_precio").on("input", () => { colorearCampos("#producto_precio") });
 
 // Monitorear cuando se intenta postear cada uno de los formularios
 // Realizar una validacion de los campos, si uno o mas de los campos estan vacios, mostrar un alerta nombrando dichos campos y debajo de dichos campos, tu nombre y apellido
-
 $("form#form_alta_categoria").on("submit", function () {
   let nombre = $.trim($("#categoria_nombre").val());
   let error = [];
-
   if (nombre == "") {
     error.push("Por favor, complete correctamente el nombre de la categoria");
   }
@@ -25,7 +22,6 @@ $("form#form_alta_categoria").on("submit", function () {
   }
   return true
 });
-
 $("form#form_alta_producto").on("submit", function () {
   let nombre = $.trim($("#producto_nombre").val());
   let descripcion = $.trim($("#producto_descripcion").val());
@@ -33,7 +29,6 @@ $("form#form_alta_producto").on("submit", function () {
   let categoria = $("#producto_categoria").val();
   let imagen = $("#producto_imagen").val();
   let error = [];
-
   if (nombre == "") {
     error.push("Por favor, cargue el nombre para su producto.");
   }
