2cedd00958ce0cdc5258cb81db4178cd7d0f3671
Agustin Gabriel Perez <perezagustin95@hotmail.com>
2023-07-09 20:57:28 -0300

    Completado PRODUCTOS.PHP

--- backend/categorias.php
+++ backend/categorias.php
@@ -2,13 +2,19 @@
 
   include '../class/autoload.php';
 
-  if(isset($_POST['action']) && $_POST['action'] == 'guardar'){
-    $miCategoria = new Categorias();
-    $miCategoria->nombre = $_POST['categoria'];
-    $miCategoria->guardar();
+  if(isset($_POST['accion']) && $_POST['action'] === 'guardar'){
+    $miProducto = new Productos();
+    $miProducto->nombre = $_POST['producto'];
+    $miProducto->descripcion = $_POST['descripcion_producto'];
+    $miProducto->precio = $_POST['precio_producto'];
+    $miProducto->categoria = $_POST['categoria_producto'];
+    $miProducto->imagen = $_POST['imagen_producto'];
+
+    $miProducto->guardar();
+
   } else if(isset($_GET['add'])){
-    include 'views/categorias.html';
+    include 'views/productos.html';
     die();
   }
-  $lista_categorias = Categorias::listar();
-  include 'views/lista_categorias.html';
+  $lista_productos = Productos::listar();
+  include 'views/lista_productos.html';
---
+++ backend/productos.php
@@ -0,0 +1,14 @@
+<?php
+
+  include '../class/autoload.php';
+
+  if(isset($_POST['accion']) && $_POST['action'] === 'guardar'){
+    $miCategoria = new Categorias();
+    $miCategoria->nombre = $_POST['categoria'];
+    $miCategoria->guardar();
+  } else if(isset($_GET['add'])){
+    include 'views/categorias.html';
+    die();
+  }
+  $lista_categorias = Categorias::listar();
+  include 'views/lista_categorias.html';
